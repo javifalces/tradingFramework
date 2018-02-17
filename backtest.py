0c1527d51b5a26362a4eb6b6ff333a45150c9f65
Javier Falces <javifalces@gmail.com>
2018-02-17 21:47:04 +0100

    fixed some value keys

--- backtest.py
+++ backtest.py
@@ -1,9 +1,14 @@
 import settings
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def analyze(backtestNameFile):
     csvFile = settings.backtestData + backtestNameFile
-    data = pd.from_csv(csvFile)
-    data['pnl'] = data['position'] * data['close'].diff()
-    data['pnl'].plot()
+    data = pd.DataFrame.from_csv(csvFile)
+    data['returns'] = data['position'] * data['close'].diff()
+    data['pnl'] = data['returns'].cumsum()
+    data['returns_benchmark'] = data['close'].diff()
+    data['pnl_benchmark'] = data['returns_benchmark'].cumsum()
+    data[['pnl', 'pnl_benchmark']].plot()
+    plt.savefig(settings.backtestPicsData + backtestNameFile + '.jpg')
+    plt.close()
--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 csvData = dir_path + os.sep + 'csvData'
 backtestData = dir_path + os.sep + 'backtestResults' + os.sep
+backtestPicsData = dir_path + os.sep + 'backtestResultsPics' + os.sep
